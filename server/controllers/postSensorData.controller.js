391af5fe768054af3c9e234a2a8a25715f7a3067
Rahul Panchal <rm5901960@gmail.com>
2024-01-06 13:41:03 +0530

    Testing tilt-sensor data

--- server/controllers/postSensorData.controller.js
+++ server/controllers/postSensorData.controller.js
@@ -1,11 +1,14 @@
 const sensordata_model = require('../models/sensordata.model')
 
 function postSensorData(req,res){
+    
     const data = req.body;
-    console.log(data);
-    sensordata_model.create(data)
+    console.log("✨✨\n",data);
+    
     if(data){
+        sensordata_model.create(data)
         res.status(200).send("🚀 Data Received By Server")
+
     }else{
         res.status(501).send("☄️ Data was not received by Server")
     }
