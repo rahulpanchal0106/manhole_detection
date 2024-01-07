2a4b266774066e75de0cb21827989c473f093675
Rahul Panchal <rm5901960@gmail.com>
2024-01-08 02:08:44 +0530

    Changing the data fetching method from database (Inefficient one)

--- server/controllers/getSensorData.controller.js
+++ server/controllers/getSensorData.controller.js
@@ -2,9 +2,11 @@ const sensordata_model = require('../models/sensordata.model');
 
 async function getSensorData (req,res){
     try{
-        const sensordata = await sensordata_model.findOne({}).sort({ timestamp: -1 });
-        console.log(sensordata.tilt,"\n",sensordata.dht)
-        res.json(sensordata)
+        // const sensordata = await sensordata_model.findOne({}).sort({ timestamp: -1 });
+        const sensordata = await sensordata_model.find({});
+        const lastData = sensordata[sensordata.length-1];
+        console.log(lastData.tilt,"\n",lastData.dht)
+        res.json(lastData)
     }catch(err){
         console.log('get sensordata error\n',err);
     }
